3b01805f52b30ead7319c1b471fafbddbba74ee7
DanyGlexMx1909 <danyboysky24@gmail.com>
2025-07-09 22:01:10 -0600

    index.php

--- index.php
+++ index.php
@@ -16,6 +16,10 @@ $productos = $conn->query("SELECT * FROM productos");
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+    integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="css/estilo.css">
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <title>Tienda de Snacks</title>
@@ -74,6 +78,26 @@ $productos = $conn->query("SELECT * FROM productos");
         </div>
       </div>
     </section>
+    
+    <!-- Redes Sociales al final de la página -->
+    <footer class="social-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-center">
+            <h3 class="social-title">¡Síguenos en nuestras redes sociales!</h3>
+            <div class="social-media-links-footer">
+              <a href="https://www.facebook.com/share/1AxW1s8x6V/" target="_blank" class="social-link-footer facebook">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+              <a href="https://www.instagram.com/zinkroshop" target="_blank" class="social-link-footer instagram">
+                <i class="fab fa-instagram"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    
     <div class="position-absolute bottom-0 end-0">
       <a class="ver-carrito btn btn-primary" href="carrito.php" target="_blank">Ver Carrito</a>
     </div>
